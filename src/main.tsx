d5a3d767f3bc79d310be4294e072107bd34b531a
PhamTuanDat <pdat100111@gmail.com>
2025-12-28 16:41:36 +0700

    feat: use absolute path with tsconfigpath

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Layout from "./layout";
+import Layout from "@/layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import BookPage from "./pages/client/book";
-import AboutPage from "./pages/client/about";
-import LoginPage from "./pages/client/auth/login";
-import RegisterPage from "./pages/client/auth/register";
-import "./styles/main.scss";
+import BookPage from "pages/client/book";
+import AboutPage from "pages/client/about";
+import LoginPage from "pages/client/auth/login";
+import RegisterPage from "pages/client/auth/register";
+import "styles/main.scss";
 
 const router = createBrowserRouter([
   {
