a0dcd31d506de91f76271002627d9526ed5e6f22
PhamTuanDat <pdat100111@gmail.com>
2025-12-28 16:14:29 +0700

    feat: base layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Layout from "./layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BookPage from "./pages/client/book";
+import AboutPage from "./pages/client/about";
+import LoginPage from "./pages/client/auth/login";
+import RegisterPage from "./pages/client/auth/register";
 
 const router = createBrowserRouter([
   {
@@ -10,21 +14,21 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/book",
-        element: <div>book</div>,
+        element: <BookPage />,
       },
       {
         path: "/about",
-        element: <div>about</div>,
+        element: <AboutPage />,
       },
     ],
   },
   {
     path: "/login",
-    element: <div>login</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>register</div>,
+    element: <RegisterPage />,
   },
 ]);
 
