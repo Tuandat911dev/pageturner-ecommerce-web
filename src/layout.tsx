afa1bb8ca3c477a57d36dba780f7e26fd6810adb
PhamTuanDat <pdat100111@gmail.com>
2025-12-29 20:08:25 +0700

    feat: make loading for AppLoading context

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,17 +4,21 @@ import AppFooter from "components/layout/app.footer";
 import { useEffect } from "react";
 import { fetchAccountAPI } from "./services/api";
 import { useCurrentApp } from "./components/context/app.context";
+import { FadeLoader } from "react-spinners";
 function Layout() {
-  const { setUser, setIsAuthenticated, setIsAppLoading } = useCurrentApp();
+  const { setUser, setIsAuthenticated, setIsAppLoading, isAppLoading } = useCurrentApp();
 
   useEffect(() => {
-    const fetchAccount = async () => {
-      const res = await fetchAccountAPI();
-      if (res.data) {
-        setUser(res.data.user);
-        setIsAuthenticated(true);
-      }
-      setIsAppLoading(false);
+    const fetchAccount = () => {
+      setIsAppLoading(true);
+      setTimeout(async () => {
+        const res = await fetchAccountAPI();
+        if (res.data) {
+          setUser(res.data.user);
+          setIsAuthenticated(true);
+        }
+        setIsAppLoading(false);
+      }, 3000);
     };
 
     fetchAccount();
@@ -22,6 +26,24 @@ function Layout() {
 
   return (
     <>
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "fixed",
+          inset: 0,
+          background: "#fff",
+          zIndex: 9999,
+          transition: "all 0.5s ease",
+          opacity: isAppLoading ? 1 : 0,
+          visibility: isAppLoading ? "visible" : "hidden",
+          pointerEvents: isAppLoading ? "auto" : "none",
+        }}
+      >
+        <FadeLoader color="#61DAFB" loading={true} height="15" />
+      </div>
       <AppHeader />
       <Outlet />
       <AppFooter />
